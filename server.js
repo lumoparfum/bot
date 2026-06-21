0ac4961bc35c1d0810ef1b9e8d988fb933b2052c
lumoparfum <mucahidtoraman55@gmail.com>
2026-06-21 17:22:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // ============================================================
 // KONFİGÜRASYON
